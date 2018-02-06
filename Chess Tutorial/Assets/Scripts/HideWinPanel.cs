d56dcbcc1e19698d40256175920022733b5aea40
afloresmorales <afloresmorales@harding.edu>
2018-02-05 23:13:59 -0600

    WinPanel pops up
    WinPanel pops up whenever a player wins.

---
+++ Chess Tutorial/Assets/Scripts/HideWinPanel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWinPanel : MonoBehaviour {
+    public void WinPanel(GameObject WinPanel)
+    {
+        WinPanel.SetActive(!WinPanel.activeSelf);
+    }
+}
