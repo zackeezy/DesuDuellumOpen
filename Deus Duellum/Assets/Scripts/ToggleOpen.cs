c7f3ba9fa5cdac3ba65c5b13c068716c0baf8708
Heather Anderson <happy2bheather@gmail.com>
2018-02-06 15:03:35 -0600

    added closable options/emotes
    added in our font (Diogenes.ttf) to the buttons and text, added specific scenes for character select and gameplay for 3 game modes, added and updated some prefabs

---
+++ Deus Duellum/Assets/Scripts/ToggleOpen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOpen : MonoBehaviour {
+
+    private bool isOpen = false;
+    public GameObject toToggle = null;
+
+    public void toggleOpen()
+    {
+        isOpen = !isOpen;
+        setVisibility();
+    }
+
+    private void setVisibility()
+    {
+        if (toToggle != null)
+        {
+            toToggle.SetActive(isOpen);
+        }
+    }
+}
