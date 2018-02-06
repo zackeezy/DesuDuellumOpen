9bdd78c5a736e0a4ab373a6e26143093d52abba8
afloresmorales <afloresmorales@harding.edu>
2018-02-05 23:38:21 -0600

    WinPanel can be toggled now
    WinPanel can be dismissed once you click on the button saying "thanks"

--- Chess Tutorial/Assets/Scripts/TogglePanelButton.cs
+++ Chess Tutorial/Assets/Scripts/TogglePanelButton.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TogglePanelButton : MonoBehaviour {
     //https://unity3d.com/learn/tutorials/modules/intermediate/live-training-archive/panels-panes-windows
+    //This toggle panel button is a universal one that can be used for any panel
     public void TogglePanel (GameObject panel){
         panel.SetActive(!panel.activeSelf);
     }
