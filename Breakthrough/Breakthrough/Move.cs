56714c5784ab87161c0b64f75041a1a51290e97e
afloresmorales <afloresmorales@harding.edu>
2018-02-08 22:03:31 -0600

    Breakthrough prototype
    added game board
    set tokens
    tokens can move, but is still missing the main logic

---
+++ Breakthrough/Breakthrough/Move.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakthrough
+{
+   public class Move : Token
+    {
+        private int fromX;
+        private int fromY;
+        private int toX;
+        private int toY;
+
+        public bool Exit { set; get; }
+
+        public Move()
+        {
+            fromX = 0;
+            fromY = 0;
+            toX = 0;
+            toY = 0;
+            Exit = false;
+        }
+
+        public void GetMove()
+        {
+            GetInput();
+
+            if (!Exit)
+            {
+                RearrangeTokens();
+            }
+        }
+        private void GetInput()
+        {
+            //get input and validate it
+            Console.WriteLine("Enter Token's from coordinate X");
+            Exit = validateInput(int.TryParse(Console.ReadLine(),out fromX));
+
+            if (!Exit)//if previous validation passed, move to the next coordinates
+            {
+                Console.WriteLine("Enter Token's from coordinate Y");
+                Exit = validateInput(int.TryParse(Console.ReadLine(), out fromY));
+            }
+            if (!Exit)//if previous validation passed, move to the next coordinates
+            {
+                Console.WriteLine("Enter Token's to coordinate X");
+                Exit = validateInput(int.TryParse(Console.ReadLine(), out toX));
+            }
+            if (!Exit)//if previous validation passed, move to the next coordinates
+            {
+                Console.WriteLine("Enter Token's to coordinate Y");
+                Exit = validateInput(int.TryParse(Console.ReadLine(), out toY));
+            }
+        }
+
+        private bool validateInput(bool parsed)
+        {
+            bool error = false;
+
+            if (!parsed)
+            {
+                error = true;
+            }
+            else if (fromX<0||fromY<0||toX<0||toY<0)
+            {
+                error = true;
+            }
+            else if(fromX>BreakthroughBoard.boardSize-1|| fromY > BreakthroughBoard.boardSize - 1 
+                || toX > BreakthroughBoard.boardSize - 1|| toY > BreakthroughBoard.boardSize - 1)
+            {
+                error = true;
+            }
+
+            if (error)
+            {
+                Console.WriteLine("Invalid input. Exiting program now.");
+            }
+
+            return error;
+        }
+
+        private void RearrangeTokens()
+        {
+            tokens[toX, toY] = tokens[fromX, fromY];
+            tokens[fromX, fromY] = SPACE;//once moved, make it null
+        }
+
+
+
+    }
+}
---
+++ Breakthrough/Breakthrough/Token.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakthrough
+{
+    public class Token
+    {
+        public const char WHITE_SYMBOL = 'W';
+        public const char BLACK_SYMBOL = 'B';
+        public const char SPACE = ' ';
+        public static char[,] tokens;
+
+        public Token()
+        {
+            //initialize
+            tokens = new char[BreakthroughBoard.boardSize, BreakthroughBoard.boardSize];
+            initialToken();
+        }
+        private void initialToken()
+        {
+            for(int i = 0; i < BreakthroughBoard.boardSize; i++)
+            {
+                for(int j =0; j < BreakthroughBoard.boardSize; j++)
+                {
+                    //place @ first two and last two rows;
+                    if(i==0||i==1)
+                    {
+                        tokens[i, j] = WHITE_SYMBOL;
+                    }
+                    else if(
+                    i == BreakthroughBoard.boardSize - 2 || i == BreakthroughBoard.boardSize - 1)
+                         {
+                            tokens[i, j] = BLACK_SYMBOL;
+                         }    
+                else
+                    {
+                        tokens[i, j] = SPACE;
+                    }
+                }
+            }
+        }
+    }
+}
