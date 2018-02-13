eb9aaca8544267954d8c75309b0b3d4c2ee3d404
Heather Anderson <happy2bheather@gmail.com>
2018-02-13 14:52:13 -0600

    fixed audio so can control volume/mute between scenes
    added dontdestroyonload() to the audio sources to keep the audio from restarting, using player preferences to set the volumes

---
+++ Deus Duellum/Assets/Scripts/MusicInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicInfo : MonoBehaviour {
+
+    public AudioSource musicSource;
+    public AudioSource effectsSource;
+    public AudioSource emotesSource;
+
+
+    void Awake () {
+		//to keep the same audio throughout, so doesn't restart music/ or reset volume
+		GameObject[] objs = GameObject.FindGameObjectsWithTag("Audio");
+		if (objs.Length > 1)
+		{
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("muted", 0) == 1)
+        {
+            AudioListener.volume = 0;
+        }
+        else if(PlayerPrefs.GetInt("muted", 0) != 1)
+        {
+            AudioListener.volume = 1;
+        }
+
+        //get the volume levels from the player preferences
+        musicSource.volume = PlayerPrefs.GetFloat("music", 1);
+        effectsSource.volume = PlayerPrefs.GetFloat("effects", 1);
+        emotesSource.volume = PlayerPrefs.GetFloat("emotes", 1);
+    }
+}
---
+++ Deus Duellum/Assets/Scripts/VolumeConnector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//connects sliders to audio sources
+public class VolumeConnector : MonoBehaviour {
+
+	public Slider musicSlider;
+	public Slider effectsSlider;
+	public Slider emotesSlider;
+    public Toggle muteToggle;
+
+    // Use this for initialization
+    void Start () {
+		//set the sliders to match the volumes from the player preferences
+		musicSlider.value = PlayerPrefs.GetFloat("music", 1);
+		effectsSlider.value = PlayerPrefs.GetFloat("effects", 1);
+		emotesSlider.value = PlayerPrefs.GetFloat("emotes", 1);
+
+        //check to see if sound is muted
+        muteToggle.isOn = (PlayerPrefs.GetInt("muted", 0) == 1);
+	}
+	
+	// Update is called once per frame
+	//update the volumes in the player preferences with the slider values
+	void Update () {
+		PlayerPrefs.SetFloat("music", musicSlider.value);
+        PlayerPrefs.SetFloat("effects", effectsSlider.value);
+        PlayerPrefs.SetFloat("emotes", emotesSlider.value);
+        if (muteToggle.isOn)
+        {
+            PlayerPrefs.SetInt("muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("muted", 0);
+        }
+    }
+}
