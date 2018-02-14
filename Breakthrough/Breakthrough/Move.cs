4a72c2cd9390f367356763fd679375f07fac6952
afloresmorales <afloresmorales@harding.edu>
2018-02-14 15:32:43 -0600

    Illegal moves are not allowed anymore.
    I am happy to announce that we have built a wall for illegal moves from tokens.

--- Breakthrough/Breakthrough/Move.cs
+++ Breakthrough/Breakthrough/Move.cs
@@ -29,7 +29,6 @@ namespace Breakthrough
             whiteToX=0;
             whiteToY=0;
 
-
             blackFromX=0;
             blackFromY=0;
             blackToX=0;
@@ -44,7 +43,7 @@ namespace Breakthrough
         {
             GetInput();
 
-            if (!Exit /*&& allowMove()*/)
+            if (!Exit)
             {
                 RearrangeTokens();
             }
@@ -123,14 +122,6 @@ namespace Breakthrough
             {
                 error = true;
             }
-            else if (blackToX == blackFromX + 1 && blackToY == blackFromY + 1)
-            {
-                error = true;
-            }
-            //else if (blackFromX == blackToX && blackFromY == blackToY)
-            //{
-            //    error = true;
-            //}
             if (error)
             {
                 Console.WriteLine("Invalid input. Exiting program now.");
@@ -155,14 +146,6 @@ namespace Breakthrough
             {
                 error = true;
             }
-            else if (whiteToX == whiteFromX - 1 && whiteToY == whiteFromY - 1)
-            {
-                error = true;
-            }
-            //else if (whiteFromX == whiteToX && whiteFromY == whiteToY)
-            //{
-            //    error = true;
-            //}
             if (error)
             {
                 Console.WriteLine("Invalid input. Exiting program now.");
@@ -178,7 +161,7 @@ namespace Breakthrough
                 //diagonal left
                 if (whiteFromX != 0 && whiteFromY != 7)
                 {
-                    if (whiteToX == whiteFromX - 1 && whiteToY == whiteFromY + 1)
+                    if (whiteToX == whiteFromX + 1 && whiteToY == whiteFromY - 1)
                     {
                         isAllowed = true;
                     }
@@ -192,9 +175,9 @@ namespace Breakthrough
                     }
                 }
                 //middle
-                if (whiteFromY != 7)
+                if (whiteFromX != 7)
                 {
-                    if (whiteToX == whiteFromX && whiteToY == whiteFromY + 1)
+                    if (whiteToX == whiteFromX+1 && whiteToY == whiteFromY)
                     {
                         isAllowed = true;
                     }
@@ -212,16 +195,17 @@ namespace Breakthrough
                     }
                 }
                 //diagonal right
-                if(blackFromX != 7 && blackFromY != 0)
+                if(blackFromX != 0 && blackFromY != 7)
                 {
-                    if (blackToX == blackFromX + 1 && blackToY == blackFromY - 1)
+                    if (blackToX == blackFromX - 1 && blackToY == blackFromY + 1)
                     {
                         isAllowed = true;
                     }
                 }
-                if(blackFromY != 0)
+                //middle
+                if(blackFromX != 0)
                 {
-                    if (blackToX == blackFromX && blackToY == blackFromY - 1)
+                    if (blackToX == blackFromX-1 && blackToY == blackFromY)
                     {
                         isAllowed = true;
                     }
@@ -233,13 +217,13 @@ namespace Breakthrough
 
         private void RearrangeTokens()
         {
-            if (isWhite)
+            if (isWhite && allowMove())
             {
                 tokens[whiteToX, whiteToY] = tokens[whiteFromX, whiteFromY];
                 tokens[whiteFromX, whiteFromY] = DASH;//once moved, place a dash in its place
                 isWhite = false;
             }
-            else
+            else if(!isWhite && allowMove())
             {
                 tokens[blackToX, blackToY] = tokens[blackFromX, blackFromY];
                 tokens[blackFromX, blackFromY] = DASH;//once moved, place a dash in its place
