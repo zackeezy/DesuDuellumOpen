a07fad5fb3801c92bf1be91d359074fd9d0f71b3
Heather Anderson <happy2bheather@gmail.com>
2018-02-18 17:29:58 -0600

    added muteother and autofade

---
+++ Deus Duellum/Assets/Scripts/MuteOther.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteOther : MonoBehaviour {
+
+    //TODO: REWRITE THIS TO GET IT TO WORK
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
