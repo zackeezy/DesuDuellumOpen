78057a3c385860851c05110ce4a07f082d8dd538
afloresmorales <afloresmorales@harding.edu>
2018-02-20 20:07:16 -0600

    ADDED MOVELOG.CS BECAUSE GITHUB HATES US

---
+++ Deus Duellum/Assets/Scripts/MoveLog.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MoveLog : MonoBehaviour {
+
+    public Text whiteLog;
+    public Text blackLog;
+
+    public Text smallWhiteLog;
+    public Text smallBlackLog;
+
+    public GameObject bigLog;
+    public GameObject smallLog;
+
+    public Text logButtonText;
+
+    private string recentWhite;
+    private string recentBlack;
+
+	// Use this for initialization
+	void Start () {
+        smallLog.SetActive(true);
+        bigLog.SetActive(false);
+        logButtonText.text = "Expand Log";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public string CoordsToNotations(int x,int y)
+    {
+        string notation = "";
+        switch (x)
+        {
+            case 0:
+                notation += "A";
+                break;
+            case 1:
+                notation += "B";
+                break;
+            case 2:
+                notation += "C";
+                break;
+            case 3:
+                notation += "D";
+                break;
+            case 4:
+                notation += "E";
+                break;
+            case 5:
+                notation += "F";
+                break;
+            case 6:
+                notation += "G";
+                break;
+            case 7:
+                notation += "H";
+                break;
+        }
+        int num = y + 1;
+        notation += num;
+        return notation;
+    }
+
+    public void ShowNotations(string notation, bool isWhite)
+    {
+        string notations;
+        if (isWhite)
+        {
+            notations = whiteLog.text;
+            notations += '\n';
+            notations += notation;
+            whiteLog.text = notations;
+            recentWhite = notation;
+            smallWhiteLog.text = recentWhite;
+        }
+        else
+        {
+            notations = blackLog.text;
+            notations += '\n';
+            notations += notation;
+            blackLog.text = notations;
+            recentBlack = notation;
+            smallBlackLog.text = recentBlack;
+        }
+    }
+
+    public void ShowWin(bool white)
+    {
+        string notations;
+        if (white)
+        {
+            notations = whiteLog.text;
+            smallWhiteLog.text += '#';
+        }
+        else
+        {
+            notations = blackLog.text;
+            smallWhiteLog.text += '#';
+        }
+
+        notations += '#';
+
+        if (white)
+        {
+           whiteLog.text = notations;
+        }
+        else
+        {
+            blackLog.text = notations;
+        }
+    }
+
+    public void ToggleLog()
+    {
+        //switch the active log
+        bigLog.SetActive(!bigLog.activeSelf);
+        smallLog.SetActive(!smallLog.activeSelf);
+        if (bigLog.activeSelf)
+        {
+            logButtonText.text = "Minimize Log";
+        }
+        else
+        {
+            logButtonText.text = "Expand Log";
+        }
+    }
+}
