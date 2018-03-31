924894ad768a89bb75f567689af7c64ed56ac325
afloresmorales <afloresmorales@harding.edu>
2018-03-31 14:20:37 -0500

    Loading scene working

---
+++ Deus Duellum/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour {
+
+    private bool loadScene = false;
+    public string LoadingSceneName;
+    public Text loadingText;
+    public Text percentage;
+    public Slider sliderBar;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Hide Slider Progress Bar in start
+       // sliderBar.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If the player has pressed the space bar and a new scene is not loading yet...
+        if (Input.GetKeyUp(KeyCode.Space) && loadScene == false)
+        {
+
+            // ...set the loadScene boolean to true to prevent loading a new scene more than once...
+            loadScene = true;
+
+            //Visible Slider Progress bar
+            sliderBar.gameObject.SetActive(true);
+
+            // ...change the instruction text to read "Loading..."
+            percentage.text = "Loading...";
+
+            // ...and start a coroutine that will load the desired scene.
+            StartCoroutine(LoadNewScene(LoadingSceneName));
+
+        }
+
+    }
+
+    // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
+    IEnumerator LoadNewScene(string sceneName)
+    {
+
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!async.isDone)
+        {
+            float progress = Mathf.Clamp01(async.progress / 0.9f);
+            sliderBar.value = progress;
+            percentage.text = progress * 100f + "%";
+            yield return null;
+
+        }
+
+    }
+}
