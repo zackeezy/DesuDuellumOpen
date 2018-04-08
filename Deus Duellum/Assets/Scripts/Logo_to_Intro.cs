7e3ca3a751b8d8cebb8eed202e31e0db5a598ae7
afloresmorales <afloresmorales@harding.edu>
2018-04-08 16:14:26 -0500

    logo scene transition added
    logo scene transition added and it is skippable

---
+++ Deus Duellum/Assets/Scripts/Logo_to_Intro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logo_to_Intro : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("DelayTransition");
+	}
+	IEnumerator DelayTransition()
+	{
+		int delayScreenTransition = 2;
+		yield return new WaitForSeconds(delayScreenTransition);
+		SceneManager.LoadScene(1);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey)
+		{
+			SceneManager.LoadScene(1);
+		}	
+	}
+}
